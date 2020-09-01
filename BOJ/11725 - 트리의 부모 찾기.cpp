4495299531717560af2b78012f410db8fb6f497d
ngwoon <todory2002@gmail.com>
2020-09-01 15:33:20 +0900

    tree 부모찾기

---
+++ BOJ/11725 - 트리의 부모 찾기.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> tree;
+vector<bool> visited;
+vector<int> parentArr;
+
+void dfs(int cur, int parent) {
+
+    printf("cur : %d, parent : %d\n", cur, parent);
+
+    if(parent != -1)
+        parentArr[cur] = parent;
+    
+    for(int i=0; i<tree[cur].size(); ++i) {
+        int child = tree[cur][i];
+        if(visited[child])
+            continue;
+
+        visited[child] = true;
+        dfs(child, cur);
+    }
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    tree.resize(n+1);
+    visited.resize(n+1,false);
+    parentArr.resize(n+1);
+    for(int i=0; i<n-1; ++i) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+
+    dfs(1, -1);
+
+    for(int i=2; i<=n; ++i)
+        printf("%d\n", parentArr[i]);
+}
