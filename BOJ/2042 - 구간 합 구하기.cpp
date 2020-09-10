94ddc66c4a724d5ceb0bb027ec96e54ec8dbcc4f
ngwoon <todory2002@gmail.com>
2020-09-10 20:56:05 +0900

    segment tree

---
+++ BOJ/2042 - 구간 합 구하기.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> num;
+
+int main(void) {
+    int n,m,k;
+    scanf("%d %d %d", &n, &m, &k);
+
+    num.resize(n);
+    for(int i=0; i<n; ++i)
+        scanf("%d", &num[i]);
+
+    
+}
