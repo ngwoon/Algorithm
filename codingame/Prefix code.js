0f210b9812660f1a4b70be410e28f969ff1ee862
ngwoon <todory2002@gmail.com>
2020-10-17 11:40:28 +0900

    prefix code

---
+++ codingame/Prefix code.js
@@ -0,0 +1,40 @@
+const table = {};
+const n = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    var inputs = readline().split(' ');
+    const b = inputs[0];
+    const c = parseInt(inputs[1]);
+    table[b] = String.fromCharCode(c);
+}
+const s = readline();
+
+function search() {
+    let decoded = "";
+    let curIdx = 0;
+    let done = true;
+    while(curIdx < s.length) {
+        let found = false;
+        for(key of Object.keys(table)) {
+            const idx = s.indexOf(key, curIdx);
+            if(idx == curIdx) {
+                decoded += table[key];
+                curIdx += key.length;
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            done = false;
+            break;
+        }
+    }
+    let retData;
+    if(done)
+        retData = decoded;
+    else
+        retData = "DECODE FAIL AT INDEX " + curIdx;
+
+    return retData;
+}
+
+console.log(search());
