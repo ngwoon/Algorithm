557401eba8d129abec362db6852eb976de4ace40
ngwoon <todory2002@gmail.com>
2021-03-10 23:53:59 +0900

    10989 - 수 정렬하기3

---
+++ BOJ/10989 - 수 정렬하기3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10001];
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int maxVal=0;
+    for(int i=0; i<n; ++i) {
+        int num;
+        scanf("%d", &num);
+        ++arr[num];
+        if(maxVal < num)
+            maxVal = num;
+    }
+
+    for(int i=1; i<=maxVal; ++i) {
+        for(int j=0; j<arr[i]; ++j)
+            printf("%d\n", i);
+    }
+}
