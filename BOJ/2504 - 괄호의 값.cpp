53973af92f50655e862f9f01e0a5cb48be24e126
ngwoon <todory2002@gmail.com>
2021-03-13 02:10:31 +0900

    괄호의 값 - 스택

---
+++ BOJ/2504 - 괄호의 값.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+stack<char> stk;
+
+int main(void) {
+    char input[31];
+    scanf("%s", input);
+    string brackets = input;
+
+    bool isWrong = false;
+    int temp = 1;
+    int result = 0;
+    for(int i=0; i<brackets.length(); ++i) {
+        char cur = brackets[i];
+        if(cur == '(' || cur == '[') {
+            temp *= cur == '(' ? 2 : 3;
+            stk.push(cur);
+        } else {
+            if(stk.empty()) {
+                isWrong = true;
+                break;
+            }
+            char before = stk.top();
+            stk.pop();
+            if((cur == ')' && before == '(') || (cur == ']' && before == '[')) {
+                if(!(brackets[i-1] == ')' || brackets[i-1] == ']'))
+                    result += temp;
+                temp /= cur == ')' ? 2 : 3;
+            } else {
+                isWrong = true;
+                break;
+            }
+        }
+    }
+
+    if(isWrong || !stk.empty())
+        printf("0");
+    else
+        printf("%d", result);
+}
