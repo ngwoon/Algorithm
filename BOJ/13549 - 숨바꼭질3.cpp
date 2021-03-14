aceff66e00ba1b5d40d762945a936b53ac6ab89c
ngwoon <todory2002@gmail.com>
2021-03-15 00:55:34 +0900

    13549 - 숨바꼭질3 해결

--- BOJ/13549 - 숨바꼭질3.cpp
+++ BOJ/13549 - 숨바꼭질3.cpp
@@ -3,50 +3,51 @@ using namespace std;
 
 int dx[2] = {-1,1};
 
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
-vector<bool> visited;
+queue<int> que;
+vector<int> visited;
 int n,k;
 
-int bfs() {
-    visited[n] = true;
-    que.push({0, n});
+void bfs() {
+    visited[n] = 0;
+    que.push(n);
     while(!que.empty()) {
-        int cnt = que.top().first;
-        int cur = que.top().second;
-
-        printf("cur : %d, cnt : %d\n", cur, cnt);
-
+        int cur = que.front();
         que.pop();
 
-        if(cur == k)
-            return cnt;
+        if(cur == k) 
+            continue;
         
         // 걷기
         for(int i=0; i<2; ++i) {
             int next = cur + dx[i];
-
+            if(k < next && i == 1)
+                continue;
             if(next < 0 || next > 100000)
                 continue;
-            if(visited[next])
+            if(visited[next] <= visited[cur] + 1)
                 continue;
 
-            visited[next] = true;
-            que.push({cnt+1, next});
+            visited[next] = visited[cur]+1;
+            que.push(next);
         }
 
+        if(k < cur)
+            continue;
+        
         int next = cur * 2;
         if(next < 0 || next > 100000)
             continue;
-        if(visited[next])   
+        if(visited[next] <= visited[cur])   
             continue;
         
-        visited[next] = true;
-        que.push({cnt, next});
+        visited[next] = visited[cur];
+        que.push(next);
     }
 }
 
 int main(void) {
     scanf("%d %d", &n, &k);
-    visited.resize(100001, false);
-    printf("%d", bfs());
+    visited.resize(100001, 100001);
+    bfs();
+    printf("%d", visited[k]);
 }
