799886820fed6d580fd0aa108b9b0c7d24748be3
ngwoon <todory2002@gmail.com>
2021-03-24 22:51:14 +0900

    Programmers 멀쩡한 사각형

---
+++ programmers/멀쩡한 사각형.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+long long solution(int w,int h) {
+    long long answer;
+    
+    if(w > h) {
+        w ^= h; h ^= w; w ^= h;
+    }
+
+    long long dw=w, dh=h;
+    int div=2;
+    while(div <= dw) {
+        if(dw % div == 0 && dh % div == 0) {
+            dw /= div; dh /= div;
+        } else
+            ++div;
+    }
+    
+    int p = w / dw;
+    long long cut = p * (dh + dw - 1);
+    if(w % dw != 0)
+        cut += (dh + dw);
+
+    answer = (long long)h * w - cut;
+
+    return answer;
+}
+
+int main(void) {
+    int a,b;
+    cin >> a >> b;
+    cout << solution(a,b);
+}
