4f4d8189bbe3c11ba1d687451679e4a6af339817
ngwoon <todory2002@gmail.com>
2021-05-03 14:02:23 +0900

    boj - 욕심쟁이 판다    DFS + DP

--- BOJ/1937 - 욕심쟁이 판다.cpp
+++ BOJ/1937 - 욕심쟁이 판다.cpp
@@ -4,39 +4,48 @@ using namespace std;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
 int forest[500][500];
-int ans, n;
+int dp[500][500];
+int n;
 
-void dfs(int y, int x, int before, int k) {
-    bool moved = false;
-    for(int i=0; i<4; ++i) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        int cur = forest[ny][nx];
-        if(nx < 0 || nx >= n || ny < 0 || ny >= n)
-            continue;
-        if(cur <= before)
-            continue;
-        
-        moved = true;
-        forest[ny][nx] = 0;
-        dfs(ny, nx, cur, k+1);
-        forest[ny][nx] = cur;
+int dfs(int y, int x) {
+    if(dp[y][x] == 0) {
+        dp[y][x] = 1;
+        bool moved = false;
+        for(int i=0; i<4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int cur = forest[ny][nx];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n)
+                continue;
+            if(cur <= forest[y][x])
+                continue;
+
+            dp[y][x] = max(dp[y][x], 1 + dfs(ny, nx));
+        }
     }
-    if(!moved)
-        ans = ans < k ? k : ans;
+    return dp[y][x];
 }
 
 int main(void) {
     scanf("%d", &n);
 
     for(int i=0; i<n; ++i) {
-        for(int j=0; j<n; ++j)
+        for(int j=0; j<n; ++j) {
             scanf("%d", &forest[i][j]);
+        }
     }
 
     for(int i=0; i<n; ++i) {
-        for(int j=0; j<n; ++j)
-            dfs(i,j,0,0);
+        for(int j=0; j<n; ++j) {
+            dfs(i,j);
+        }
+    }
+
+    int ans=0;
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            ans = ans < dp[i][j] ? dp[i][j] : ans;
+        }
     }
 
     printf("%d", ans);
