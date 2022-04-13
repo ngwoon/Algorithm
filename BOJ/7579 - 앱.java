819fe4a4ac0f73db8dcd7be1c044b08900d71e2c
ngwoon <todory2002@gmail.com>
2022-04-13 14:09:42 +0900

    백준 - 7579 - 앱 (dp, 냅색)

--- BOJ/7579 - 앱.java
+++ BOJ/7579 - 앱.java
@@ -17,7 +17,7 @@ import static java.lang.Integer.parseInt;
         dp[i][w] = i개의 보석이 있고 배낭의 무게 한도가 w일 때 도둑이 얻을 수 있는 최적의 이익
     냅색 문제의 점화식은 아래와 같다.
         * 만약 i번째 보석의 무게가 w보다 무거우면, 가방에 넣을 수 없으므로 dp[i][w] = dp[i-1][w] 이다.
-        * 만약 i번째 보석의 무게각 w보다 가벼우면, dp[i][w] = max(dp[i-1][w-weight[i]] + cost[i], dp[i][w]) 이다.
+        * 만약 i번째 보석의 무게각 w보다 가벼우면, dp[i][w] = max(dp[i-1][w-weight[i]] + cost[i], dp[i-1][w]) 이다.
 
     이 냅색 알고리즘을 현재 문제에 적용해보자.
     보석 = 앱, 비활성화 비용 = 가방 무게, 메모리 = 보석 가치 으로 해석해볼 수 있다.
@@ -25,7 +25,7 @@ import static java.lang.Integer.parseInt;
         dp[i][c] = i개의 앱이 있고 비활성화 비용의 한도를 c로 정했을 때, 확보할 수 있는 최대 메모리의 크기
     점화식은 아래와 같이 쓸 수 있을 것이다.
         * 만약 i번째 앱의 비활성화 비용이 c보다 무거우면, 조건을 만족할 수 없으므로 dp[i][c] = dp[i-1][c] 이다.
-        * 만약 i번째 앱의 비활성화 비용이 c보다 가벼우면, dp[i][c] = max(dp[i-1][c-cost[i]] + memory[i], dp[i][c]) 이다.
+        * 만약 i번째 앱의 비활성화 비용이 c보다 가벼우면, dp[i][c] = max(dp[i-1][c-cost[i]] + memory[i], dp[i-1][c]) 이다.
  */
 
 class App {
